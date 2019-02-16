c6258d5f46d32bae4a07c2003134d569d343fc12
awave1 <artgolovin96@gmail.com>
2019-02-16 16:59:43 -0700

    Statically serve react app on prod

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,16 @@
 const Koa = require('koa');
+const serve = require('koa-static');
+const path = require('path');
 
 const app = new Koa();
 const PORT = process.env.PORT || 8000;
 
-app.use(async ctx => (ctx.body = { success: true }));
+// app.use(async ctx => (ctx.body = { success: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  const publicPath = path.resolve(__dirname, '../../client/build');
+  console.log(`Serving: ${publicPath}`);
+  app.use(serve(publicPath));
+}
 
 app.listen(PORT);
