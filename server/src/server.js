3eec7eb0e09cdc639a04c3e2fcc693d3fd12be66
awave1 <artgolovin96@gmail.com>
2019-02-16 18:43:09 -0700

    add prod

--- server/src/server.js
+++ server/src/server.js
@@ -5,12 +5,10 @@ const path = require('path');
 const app = new Koa();
 const PORT = process.env.PORT || 8000;
 
-// app.use(async ctx => (ctx.body = { success: true }));
-
-const publicPath = path.resolve(__dirname, '../../client/build');
-console.log(`Serving: ${publicPath}`);
-app.use(serve(publicPath));
-// if (process.env.NODE_ENV === 'production') {
-// }
+if (process.env.NODE_ENV === 'production') {
+  const publicPath = path.resolve(__dirname, '../../client/build');
+  console.log(`Serving: ${publicPath}`);
+  app.use(serve(publicPath));
+}
 
 app.listen(PORT);
