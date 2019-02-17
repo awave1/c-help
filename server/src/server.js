4a40a03e355b87eab1c09715c51cc49d6218333c
Daniel <daniel.artuso1@gmail.com>
2019-02-17 15:11:32 -0700

    port changes

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,11 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3131;
-app.listen(PORT);
+app.listen(process.env.PORT || 3000, function() {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env
+  );
+});
 
 console.log(`Running: localhost:${PORT}`);
