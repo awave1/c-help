cac9302743be8286b2618eee091ae293bde35de4
Alex Zecevic <alex.z@live.ca>
2019-02-17 02:17:51 -0700

    WOOOW look at this color xDDDDD

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <style>{'body { background-color: lightblue; }'}</style>
         <Header />
         <Middle />
         <Footer />
