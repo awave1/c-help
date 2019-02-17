425f39243e0f6d6cd4161c7c9d48219bdb4d6ebc
awave1 <artgolovin96@gmail.com>
2019-02-17 12:58:01 -0700

    Add 3hr weather changes

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const styles = {
+  card: {
+    minWidth: 125,
+    margin: 15,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
+const WeatherWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 class Weather extends Component {
   constructor(props) {
     super(props);
     this.state = {
       weather: '',
+      forecast: [],
     };
 
     this.getWeather = this.getWeather.bind(this);
@@ -20,6 +49,7 @@ class Weather extends Component {
 
     this.setState({
       weather: data.weather,
+      forecast: data.forecast,
     });
   }
 
@@ -28,7 +58,48 @@ class Weather extends Component {
   }
 
   render() {
-    return <div>{this.state.weather}</div>;
+    const { classes } = this.props;
+    const point = <span className={classes.bullet}>•</span>;
+
+    const forecast = this.state.forecast.map(f => (
+      <Card className={classes.card} key={f.date.stringValue}>
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            <b>
+              {f.description.stringValue.charAt(0).toUpperCase() +
+                f.description.stringValue}
+            </b>
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            {new Date(f.date.stringValue).toLocaleDateString()}
+            <br />
+            {new Date(f.date.stringValue).toLocaleTimeString()}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            <b>Current: </b>
+            {f.temp.structValue.fields.current.numberValue}
+            <br />
+            <b>Min: </b>
+            {f.temp.structValue.fields.min.numberValue}
+            <br />
+            <b>Max: </b>
+            {f.temp.structValue.fields.max.numberValue}
+            <br />
+          </Typography>
+        </CardContent>
+      </Card>
+    ));
+
+    return (
+      <WeatherWrapper>
+        <div style={{ alignSelf: 'center' }}>{this.state.weather}</div>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>{forecast}</div>
+      </WeatherWrapper>
+    );
   }
 }
-export default Weather;
+export default withStyles(styles)(Weather);
